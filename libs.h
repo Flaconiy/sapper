6202f9f611592a0160f7693350ffc2e59114ae3e
Flaconiy <44830769+Flaconiy@users.noreply.github.com>
2020-11-29 02:08:34 +0300

    added lib

---
+++ libs.h
@@ -0,0 +1,10 @@
+#ifndef LIBS_H
+#define LIBS_H
+
+#include "../Graph_lib/Graph.h"
+#include "../Graph_lib/Window.h"
+#include "../Graph_lib/GUI.h"
+#include "../Graph_lib/fltk.h"
+#include "../Graph_lib/Point.h"
+
+#endif // LIBS_H
